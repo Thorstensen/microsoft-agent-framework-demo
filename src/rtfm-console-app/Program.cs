b098918b082830ab981795b71a1151f08a71b162
Tobias Moe Thorstensen <tobias.thorstensen@fortedigital.com>
2026-04-08 23:02:51 +0200

    added a tool to read all MD files

--- src/rtfm-console-app/Program.cs
+++ src/rtfm-console-app/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+using Azure.AI.Projects;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using rtfm_console_app;
+
+var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT")
+               ?? throw new InvalidOperationException("Set AZURE_OPENAI_ENDPOINT");
+
+var modelName = Environment.GetEnvironmentVariable("AZURE_OPENAI_MODEL_NAME") ?? "gpt-4o-mini";
+
+AIAgent agent = new AIProjectClient(
+        new Uri(endpoint),
+        new DefaultAzureCredential()
+    )
+    .AsAIAgent(
+        model: modelName,
+        instructions: "You are a friendly helpful assistant answering questions about documentation. " +
+                      "Your users are typically developers, software architects or other technically savvy people. " +
+                      "You should answer questions about the documentation as concisely as possible, and if you don't know the answer, say you don't know." +
+                      "" +
+                      "You use tools ",
+        name: "DocsAssistant",
+        tools: [AIFunctionFactory.Create(Tools.GetDocumentation)]
+    );
+
+Console.WriteLine("Ask me anything about the documentation!");
+Console.WriteLine(Environment.NewLine);
+var question = Console.ReadLine();
+
+Console.WriteLine(await agent.RunAsync());
---
+++ src/rtfm-console-app/Tools.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel;
+using System.Text;
+
+namespace rtfm_console_app;
+
+public static class Tools
+{
+    [Description("Read all documentation.")]
+    public static string GetDocumentation()
+    {
+        var sb = new StringBuilder();
+
+        foreach (var file in Directory.GetFiles("../docs", "*.md", SearchOption.AllDirectories))
+        {
+            sb.AppendLine($"// --- {Path.GetFileName(file)} ---");
+            sb.AppendLine(File.ReadAllText(file));
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
+    }
+}
