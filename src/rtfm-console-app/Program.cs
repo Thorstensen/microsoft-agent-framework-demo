a23d7cfccf901ec0fa7dafc1eb02a1da5e3bafb8
Tobias Moe Thorstensen <tobias.thorstensen@fortedigital.com>
2026-04-09 07:53:46 +0200

    added instructions as a const

--- src/rtfm-console-app/Program.cs
+++ src/rtfm-console-app/Program.cs
@@ -7,17 +7,18 @@ using rtfm_console_app;
 var endpoint = GetArguments(args, "--endpoint") ?? throw new ArgumentException("endpoint needs to point to azure foundry service");
 var modelName = GetArguments(args, "--model") ?? "gpt-4o-mini";
 
+const string instructions = "You are a friendly helpful assistant answering questions about documentation. " +
+                            "Your users are typically developers, software architects or other technically savvy people. " +
+                            "You should answer questions about the documentation as concisely as possible, " +
+                            "and if you don't know the answer, say you don't know.";
+
 AIAgent agent = new AIProjectClient(
         new Uri(endpoint),
         new DefaultAzureCredential()
     )
     .AsAIAgent(
         model: modelName,
-        instructions: "You are a friendly helpful assistant answering questions about documentation. " +
-                      "Your users are typically developers, software architects or other technically savvy people. " +
-                      "You should answer questions about the documentation as concisely as possible, and if you don't know the answer, say you don't know." +
-                      "" +
-                      "You use tools ",
+        instructions: instructions,
         name: "DocsAssistant",
         tools: [AIFunctionFactory.Create(Tools.GetDocumentation)]
     );
