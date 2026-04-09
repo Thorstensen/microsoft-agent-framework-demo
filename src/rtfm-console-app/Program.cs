6dbef3df215958f4cbd6f65fa5ec7f1d5b0a5571
Tobias Moe Thorstensen <tobias.thorstensen@fortedigital.com>
2026-04-09 07:54:48 +0200

    better readability

--- src/rtfm-console-app/Program.cs
+++ src/rtfm-console-app/Program.cs
@@ -7,10 +7,13 @@ using rtfm_console_app;
 var endpoint = GetArguments(args, "--endpoint") ?? throw new ArgumentException("endpoint needs to point to azure foundry service");
 var modelName = GetArguments(args, "--model") ?? "gpt-4o-mini";
 
-const string instructions = "You are a friendly helpful assistant answering questions about documentation. " +
-                            "Your users are typically developers, software architects or other technically savvy people. " +
-                            "You should answer questions about the documentation as concisely as possible, " +
-                            "and if you don't know the answer, say you don't know.";
+const string instructions =
+    """
+    You are a friendly helpful assistant answering questions about documentation.
+    Your users are typically developers, software architects or other technically savvy people.
+    You should answer questions about the documentation as concisely as possible,
+    and if you don't know the answer, say you don't know.
+    """;
 
 AIAgent agent = new AIProjectClient(
         new Uri(endpoint),
